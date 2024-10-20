a913c5556cba15828f1ceb09cdf04a908fa66cd4
Roberto Nares <roberto.nares@gmail.com>
2024-10-19 20:39:00 -0700

    Add column validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # Presence validations
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  # Length validations
+  validates :username, length: { in: 4..12 }
+  validates :password, length: { in: 6..16 }
+
+  # Format validations
+  validates :email, format: { with: /\A[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}\z/ }
+
+  # Uniqueness validations
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
 end
